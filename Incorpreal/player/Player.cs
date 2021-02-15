2aec1179ab0becdeae2b8c954490d68e4331a165
dassit-sys <elijaheason2020@gmail.com>
2021-02-15 18:08:20 -0500

    Added Area2D to player and enemies/walls groupings for use with possession

--- Incorpreal/player/Player.cs
+++ Incorpreal/player/Player.cs
@@ -1,11 +1,14 @@
 using Godot;
 using System;
+using System.Collections.Generic;
+using Godot.Collections;
 
 public class Player : KinematicBody2D {
     [Export]
 
     public int moveSpeed = 250;
     public CollisionShape2D hitbox;
+    public Sprite playerSpriteNode;
 
     // For all the methods pertaining to stats, nothing is set in stone
     // numbers are expected to change as at a later date.
@@ -40,6 +43,8 @@ public class Player : KinematicBody2D {
         MaxHealth = 5 + Vitality;
         CurrentHealth = MaxHealth;
         Level = 1;
+
+
     }
 
     public Player() {
@@ -105,9 +110,6 @@ public class Player : KinematicBody2D {
         var motion = new Vector2();
         //Player will use WASD to move their character
         
-        // Adding animation code from Elijah's branch.
-        animate = (AnimationPlayer) GetNode("AnimationPlayer");
-        
         motion.x = Input.GetActionStrength("move_right") - Input.GetActionStrength("move_left");
         motion.y = Input.GetActionStrength("move_down") - Input.GetActionStrength("move_up");
 
@@ -123,16 +125,66 @@ public class Player : KinematicBody2D {
         */
         var collision = MoveAndCollide(motion.Normalized() * delta * moveSpeed);
         
-        
-        
         if (collision != null) {
             if (collision.Collider.HasMethod("Hit")) {
-                ChangeState("dead");
+                //ChangeState("dead");
                 collision.Collider.Call("Hit");
             }
-        }        
+        }     
+
+        // Adding animation code from Elijah's branch
+        animate = (AnimationPlayer) GetNode("AnimationPlayer");
+        playerSpriteNode = (Sprite) GetNode("Sprite");
+
+        //Possession listener
+        if (Input.IsActionPressed("possession")) { //If Alt is pressed
+            Possess();
+        }
     }
     
+    public void Possess() {
+        //Check if anyone within range
+        Area2D possessionArea = (Area2D) GetNode("Area2D");
+        Godot.Collections.Array nearby = possessionArea.GetOverlappingBodies(); //Check who is nearby
+        GD.Print("count:", nearby.Count); //For debugging
+        if (nearby.Count > 1) { //The player counts as one
+
+            float closestDistance = 1000;
+            int closestEnemyIndex = 0;
+            Boolean enemyFound = false;
+
+            //2. Find closest enemy
+            for (int x = 0; x < nearby.Count; x++) {
+                PhysicsBody2D currentEnemy = (PhysicsBody2D) nearby[x]; //Grab one
+                if (currentEnemy.Name != "Player" && currentEnemy.GetGroups().Contains("Enemies")) { //Prevent possessing yourself OR an inanimate object
+                    float currentDistance = currentEnemy.GlobalPosition.DistanceTo(this.GlobalPosition); //Calculate distance
+                    if (currentDistance < closestDistance) { //Check if closer than current closest
+                        closestEnemyIndex = x;
+                        closestDistance = currentDistance;
+                        enemyFound = true;
+                        //GD.Print("Closest now: ", ((PhysicsBody2D)nearby[x]).Name); //For debugging
+                    }
+                }
+            }  
+
+            //3. Possess if enemy found
+            if (enemyFound) {
+                //Change state to possession state
+                ChangeState("possession");
+                //Grab victim
+                KinematicBody2D victim = (KinematicBody2D) nearby[closestEnemyIndex];
+                //Grab victim sprite
+                Sprite victimSprite = (Sprite) victim.GetNode("Sprite");
+                //Set player sprite to that of victim
+                if (victimSprite != null && victim != null && playerSpriteNode != null) { //Prevent NullPointers
+                    GD.Print("Inside");
+                    GetNode(Sprite).Texture = victimSprite.Texture; //Stopped here
+//                    playerSpriteNode.Texture = victimSprite.Texture;    
+                }
+            }
+        }
+    }
+
     // Imported from Elijah's branch, and matched names, and styles
     public void ChangeState(string newState) {
       switch (newState) {
@@ -141,18 +193,19 @@ public class Player : KinematicBody2D {
           break;
         }
         case "dead": {
-          animate.Stop();
           animate.Play("Die");
           break;
         }
         case "walk": {
-          animate.Stop();
           animate.Play("Walking");
           break;
         }
-        default: {
-          break;
+        case "possession": {
+            break;
         }
-      }
+        default: {
+            break;
+        }
+        }
     }
 }
