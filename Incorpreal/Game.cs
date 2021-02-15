2de4907fea7b182a855fdcc95a789d0ec45e9a6d
Austin Montgomery <austinhunterm@gmail.com>
2021-02-15 00:03:45 -0500

    Audio Player Works
    I did it guys!

--- Incorpreal/Game.cs
+++ Incorpreal/Game.cs
@@ -5,14 +5,15 @@ public class Game : Node
 {
 	// Declare member variables here. Examples:
 	// private int a = 2;
-	// private string b = "text";
-	AudioStreamPlayer audioStreamPlayer= new AudioStreamPlayer();
+	public AudioStreamPlayer audioStreamPlayer = new AudioStreamPlayer();
 	const string Path = "res://sounds/test.wav";
+	// private string b = "text";
 	// Called when the node enters the scene tree for the first time.
 	public override void _Ready()
 	{
-		audioStreamPlayer.Stream = ((AudioStream)GD.Load(Path));
-		audioStreamPlayer.VolumeDb = (-10);
+		this.AddChild(audioStreamPlayer);
+		AudioStream Background = (AudioStream)GD.Load(Path);
+		audioStreamPlayer.Stream = Background;
 		audioStreamPlayer.Play();
 	}
 
