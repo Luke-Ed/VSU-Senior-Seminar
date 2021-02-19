50b2c245522ae15742ea471b912be9cc7a6afb7e
Brendan <brockbrendan0@gmail.com>
2021-02-18 21:15:28 -0500

    Finish base for Map 1, edit movement speed, etc

--- Incorpreal/player/Player.cs
+++ Incorpreal/player/Player.cs
@@ -4,7 +4,7 @@ using System;
 public class Player : KinematicBody2D {
 	[Export]
 
-	public int moveSpeed = 250;
+	public int moveSpeed = 125;
 	public CollisionShape2D hitbox;
 
 	// For all the methods pertaining to stats, nothing is set in stone
