7a9fe2e2877fba28b349dc631158a16a605efce2
Maverick Engan <maverickengan@gmail.com>
2021-02-21 12:03:05 -0500

    Going back to a saved version before attempting fixing

--- Incorpreal/Battle/Battle.cs
+++ Incorpreal/Battle/Battle.cs
@@ -20,7 +20,7 @@ public class Battle : Node
     //This is just for demo expamples to not get stuck on battle screen.
     public void _on_Button_pressed()
     {
-       GetTree().ChangeScene("res://Level 1.tscn");
+       GetTree().ChangeScene("res://Game.tscn");
     }
 
     public void _on_TakeDamageButton_pressed()
