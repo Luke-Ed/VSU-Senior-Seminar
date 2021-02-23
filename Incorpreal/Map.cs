cf973d12739a4315eadc1d92d9f2faa5c62813f0
dassit-sys <elijaheason2020@gmail.com>
2021-02-23 13:29:42 -0500

    Readded SpawnEnemy(), this time to pre-existing map.cs

--- Incorpreal/Map.cs
+++ Incorpreal/Map.cs
@@ -8,4 +8,12 @@ public class Map : Node2D
     {
         
     }
+
+    public void SpawnEnemy(string resPath, Vector2 position, Node currentScene) {
+        PackedScene enemyScene = (PackedScene) ResourceLoader.Load(resPath); //Load resource
+        KinematicBody2D enemy = (KinematicBody2D)enemyScene.Instance(); //Instantiate
+        currentScene.AddChild(enemy); //Add to scene
+        enemy.GlobalPosition = position; //Set to original position
+        //enemy.ChangeState("confused"); //Doesn't exist yet
+    }
 }
