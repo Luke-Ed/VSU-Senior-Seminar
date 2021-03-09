14528fb2779cf9ed9382bba0dae61c3af99b8c40
Brendan <brockbrendan0@gmail.com>
2021-03-09 15:16:28 -0500

    On last phase of map interaction. Starting map 2.

--- Incorpreal/TileSets/Interactables.cs
+++ Incorpreal/TileSets/Interactables.cs
@@ -8,6 +8,9 @@ public class Interactables : TileMap
     //Establish loot area
     PackedScene loot_area = GD.Load<PackedScene>("res://TileSets/LootArea1.tscn");
 
+    //Array to hold the literal tile and the texture on that tile
+    Godot.Collections.Array usedTileAndTextures = new Godot.Collections.Array();
+
     // Called when the node enters the scene tree for the first time.
     public override void _Ready()
     {
@@ -18,7 +21,7 @@ public class Interactables : TileMap
         Vector2 closed_chest_silver = new Vector2(0, 0);
         Vector2 closed_chest_gold = new Vector2(2, 0);
 
-        //Return an array of all cells with the given tile id
+        //Return an array of all cells (array of Vector2) with the given tile id
         Godot.Collections.Array used_chest_tiles = GetUsedCellsById(22);
 
         //Loop through each Vector2 to check if the chest is using the 'Closed' texture
@@ -27,6 +30,8 @@ public class Interactables : TileMap
             if (GetCellAutotileCoord((int)cur_tile[0], (int)cur_tile[1]).Equals(closed_chest_silver) || GetCellAutotileCoord((int)cur_tile[0], (int)cur_tile[1]).Equals(closed_chest_gold)) {
                 Area2D loot_area_instance = (Area2D)loot_area.Instance();
                 loot_area_instance.Position = MapToWorld(cur_tile);
+                usedTileAndTextures.Add((Vector2)cur_tile);
+                usedTileAndTextures.Add((Vector2)GetCellAutotileCoord((int)cur_tile[0], (int)cur_tile[1]));
                 AddChild(loot_area_instance);
             }
         }
--- Incorpreal/TileSets/Interaction.cs
+++ Incorpreal/TileSets/Interaction.cs
@@ -6,6 +6,11 @@ public class Interaction : CanvasLayer
     //Our "state" for player interaction with map objects
     String action_state = "off";
 
+    //Variables to hold current tile texture and location
+    Vector2 tile;
+    Vector2 tile_region;
+    Vector2 flip_tile = new Vector2(1,0);
+
     public override void _Process(float delta) {
         if(Input.IsActionJustPressed("interact")) {
             //Respond based on the action_state
@@ -27,6 +32,8 @@ public class Interaction : CanvasLayer
     //When the player enters a loot area, print "Looting"
     public void OnLootAreaEntered(Area2D area) {
         action_state = "on";
+        //tile = usedTileAndTextures[0];
+        //tile_region = usedTileAndTextures[1] + flip_tile;
         GD.Print("Looting");
     }
 
