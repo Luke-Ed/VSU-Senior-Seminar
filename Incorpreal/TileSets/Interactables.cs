f3d055903061e51a968c86d561ad53d44e15085a
Brendan <brockbrendan0@gmail.com>
2021-03-09 22:09:16 -0500

    Start on code for disabling chest after use.

--- Incorpreal/TileSets/Interactables.cs
+++ Incorpreal/TileSets/Interactables.cs
@@ -9,13 +9,16 @@ public class Interactables : TileMap
     PackedScene loot_area = GD.Load<PackedScene>("res://TileSets/LootArea1.tscn");
 
     //Array to hold the literal tile and the texture on that tile
-    Godot.Collections.Array usedTileAndTextures = new Godot.Collections.Array();
+    Godot.Collections.Array<Vector2> usedTileAndTextures = new Godot.Collections.Array<Vector2>();
+
+    Interaction conPrint = new Interaction();
 
     // Called when the node enters the scene tree for the first time.
     public override void _Ready()
     {
         //Establish Signal Connection with CanvasLayer elements
-        CanvasLayer test_looting = (CanvasLayer)GetNode("../Interaction_Console");
+        CanvasLayer output_console = (CanvasLayer)GetNode("../Interaction_Console");
+        Interaction inter = GetNode("../Interaction_Console") as Interaction;
 
         //Vector2 to represent 'Closed' chest texture
         Vector2 closed_chest_silver = new Vector2(0, 0);
@@ -24,7 +27,7 @@ public class Interactables : TileMap
         //Return an array of all cells (array of Vector2) with the given tile id
         Godot.Collections.Array used_chest_tiles = GetUsedCellsById(22);
 
-        //Loop through each Vector2 to check if the chest is using the 'Closed' texture
+        //Loop through each Vector2 to check if the chest is using the 'Closed' textures
         foreach (Vector2 cur_tile in used_chest_tiles)
         {
             if (GetCellAutotileCoord((int)cur_tile[0], (int)cur_tile[1]).Equals(closed_chest_silver) || GetCellAutotileCoord((int)cur_tile[0], (int)cur_tile[1]).Equals(closed_chest_gold)) {
@@ -45,8 +48,10 @@ public class Interactables : TileMap
         //Signals are "body_entered" and "body_exited"
         foreach (Area2D area in signal_radius)
         {
-            area.Connect("body_entered", test_looting, "OnLootAreaEntered");
-            area.Connect("body_exited", test_looting, "OnLootAreaExited");
+            area.Connect("body_entered", output_console, "OnLootAreaEntered");
+            area.Connect("body_exited", output_console, "OnLootAreaExited");
         }
+
+        inter.setUsedTiles(usedTileAndTextures);
     }
 }
--- Incorpreal/TileSets/Interaction.cs
+++ Incorpreal/TileSets/Interaction.cs
@@ -3,13 +3,21 @@ using System;
 
 public class Interaction : CanvasLayer
 {
-    //Our "state" for player interaction with map objects
+    //Our "state" for player interaction with map objects,
+    //Will likely be replaced by an Enum as code grows more complex
     String action_state = "off";
+
+    //
 
     //Variables to hold current tile texture and location
     Vector2 tile;
     Vector2 tile_region;
-    Vector2 flip_tile = new Vector2(1,0);
+    Vector2 flip_tile = new Vector2(2,0);
+    Godot.Collections.Array<Vector2> usedTiles = new Godot.Collections.Array<Vector2>();
+
+    public Interaction() {
+
+    }
 
     public override void _Process(float delta) {
         if(Input.IsActionJustPressed("interact")) {
@@ -18,22 +26,31 @@ public class Interaction : CanvasLayer
                 case "off":
                     GD.Print("I got nothing to do here.");
                     break;
+
                 case "on":
                     //Add some function or load a scene/panel for gold, etc.
                     GD.Print("What do we have here?");
+                    TileMap changeMap = (TileMap)GetNode("../Interactables");
+                    changeMap.SetCell((int)tile[0], (int)tile[1], 22, false, false, false, tile_region);
+                    //SetCell(int x, int y, int tile, boolean flip_x, boolean flip_y, boolean transpose, Vector2 autotileCoordinates)
                     break;
+
                 default:
                     GD.Print("...");
                     break;
             }
         }
     }
+
+    public void setUsedTiles(Godot.Collections.Array<Vector2> tiles) {
+        usedTiles = tiles;
+    }
     
     //When the player enters a loot area, print "Looting"
     public void OnLootAreaEntered(Area2D area) {
         action_state = "on";
-        //tile = usedTileAndTextures[0];
-        //tile_region = usedTileAndTextures[1] + flip_tile;
+        tile = (Vector2)usedTiles[0];
+        tile_region = (Vector2)usedTiles[1] + flip_tile;
         GD.Print("Looting");
     }
 
