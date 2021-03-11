4b83a494ad1e970bbd6f4c8aa18c03556c84da20
Brendan Brock <36016421+AedenSpear@users.noreply.github.com>
2021-03-11 11:47:42 -0500

    Fix accidental change to Player.cs

--- Incorpreal/player/Player.cs
+++ Incorpreal/player/Player.cs
@@ -21,7 +21,7 @@ public class Player : KinematicBody2D {
 
     //Can create two different types of players one with melee stats and the other with ranged.
     //Will be able choose class at the start of the game at a main menu once implemented.
-    public int Strength, Dexterity, Vitality, Intelligence, Luck, Experience, MaxHealth, CurrentHealth, Level, AttackDamage;
+    public int Strength, Dexterity, Vitality, Intelligence, Luck, Experience, MaxHealth, CurrentHealth, Level, AttackDamage, ExperienceToNextLevel;
     public String CharacterClass;
     
     public Player(String Class)
