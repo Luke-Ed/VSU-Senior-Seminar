396e487344de6989fb6660e5c832acfdf2dada2a
Brendan <brockbrendan0@gmail.com>
2021-03-21 22:25:19 -0400

    Clean up interaction code. Start map 2 base.

--- Incorpreal/TileSets/Interactables.cs
+++ Incorpreal/TileSets/Interactables.cs
@@ -8,15 +8,11 @@ public class Interactables : TileMap
     //Load the Area2D scene that represents lootable areas
     PackedScene loot_area = GD.Load<PackedScene>("res://TileSets/LootArea1.tscn");
 
-    //Array to hold a Vector2 representation of the tile and the texture on that tile
-    Godot.Collections.Array<Vector2> usedTileAndTextures = new Godot.Collections.Array<Vector2>();
-
     // Called when the node enters the scene tree for the first time.
     public override void _Ready()
     {
         //Establish Signal Connection with CanvasLayer elements
         //This allows us to print to console or give other responses to Signals and Responses
-        CanvasLayer output_console = (CanvasLayer)GetNode("../Interaction_Console");
         Interaction inter = GetNode("../Interaction_Console") as Interaction; //This miracle line of code says "load node A as a script"
 
         //Vector2 to represent 'Closed' chest textures
@@ -34,8 +30,8 @@ public class Interactables : TileMap
                 loot_area_instance.Position = MapToWorld(cur_tile);
                 //MapToWorld(Vector2) - Returns the global position corresponding to the given tilemap's (grid-based) coordinates.
 
-                usedTileAndTextures.Add((Vector2)cur_tile);
-                usedTileAndTextures.Add((Vector2)GetCellAutotileCoord((int)cur_tile[0], (int)cur_tile[1]));
+                inter.addUsedTiles((Vector2)cur_tile);
+                inter.addUsedTextures((Vector2)GetCellAutotileCoord((int)cur_tile[0], (int)cur_tile[1]));
                 //GetCellAutoTileCoord(int x, int y) - Returns a Vector2 coordinate of the tile using a specific texture in the current tileset
 
                 AddChild(loot_area_instance);
@@ -55,8 +51,5 @@ public class Interactables : TileMap
             area.Connect("body_exited", inter, "OnLootAreaExited");
             //Connect(String signal, Node signal_handler, String method_name)
         }
-
-        //Poor boy, all on his lonesome. Need to pass attributes here in C# cause GDScript doesn't. IDFK man.
-        inter.setUsedTiles(usedTileAndTextures);
     }
 }
