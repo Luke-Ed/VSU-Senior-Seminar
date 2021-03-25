8d87d1b6813021b34165999011460f11d125f070
Brendan <brockbrendan0@gmail.com>
2021-03-25 14:43:54 -0400

    Last commit before next presentation.

--- Incorpreal/TileSets/Interaction.cs
+++ Incorpreal/TileSets/Interaction.cs
@@ -7,6 +7,7 @@ public class Interaction : CanvasLayer
     
     //Our "state" for player interaction with map objects,
     //Will likely be replaced by an Enum as code grows more complex
+    //Enum states = {Idle, Looting, Reading, Puzzle, Secret}
     String action_state = "off";
 
     //The loot_areas to be disabled after the chest opens
@@ -16,7 +17,7 @@ public class Interaction : CanvasLayer
     // Chests are loaded from left to right, top to bottom. 
     // Chests in the upper left corner are loaded before lower right.
     // Tracking passed textures works out better than number of chests!
-    int chest_texture = 0;
+    int current_chest = 0;
 
     //Variables to hold current tile texture and location
     Vector2 tile;
@@ -35,21 +36,21 @@ public class Interaction : CanvasLayer
             //Respond based on the action_state
             switch(action_state) {
                 case "off":
-                    GD.Print("I got nothing to do here.");
+                    GD.Print("I got nothing to do here. \n");
                     break;
 
                 case "on":
                     //Add some function or load a scene/panel for gold, etc.
-                    GD.Print("What do we have here?");
+                    GD.Print("What do we have here? \n");
                     TileMap changeMap = (TileMap)GetNode("../Interactables");
                     changeMap.SetCell((int)tile[0], (int)tile[1], 22, false, false, false, tile_region);
                     //SetCell(int x, int y, int tile, boolean flip_x, boolean flip_y, boolean transpose, Vector2 autotileCoordinates)
 
-                    CollisionShape2D disable_chest = (CollisionShape2D)(GetNode(((String)(loot_areas[chest_texture].GetPath() + "/CollisionShape2D"))));
+                    CollisionShape2D disable_chest = (CollisionShape2D)(GetNode(((String)(loot_areas[current_chest].GetPath() + "/CollisionShape2D"))));
                     disable_chest.Disabled = true;
                     //Disable the collision box for this chest
 
-                    chest_texture++;
+                    current_chest++;
                     //Switch to next chest
                     break;
 
@@ -77,14 +78,14 @@ public class Interaction : CanvasLayer
     //When the player enters a loot_area, print "Looting"
     public void OnLootAreaEntered(Area2D area) {
         action_state = "on";
-        tile = (Vector2)usedTiles[chest_texture]; //Pass the current tile ingame to the process method
-        tile_region = (Vector2)usedTextures[chest_texture] + open_chest; //Pass the Vector2 and change the texture to the "open chest" texture
-        GD.Print("Looting");
+        tile = (Vector2)usedTiles[current_chest]; //Pass the current tile ingame to the process method
+        tile_region = (Vector2)usedTextures[current_chest] + open_chest; //Pass the Vector2 and change the texture to the "open chest" texture
+        // GD.Print("Looting \n");
     }
 
     //When the player leaves a loot_area, print "Leaving"
     public void OnLootAreaExited(Area2D area) {
         action_state = "off";
-        GD.Print("Leaving");
+        // GD.Print("Leaving \n");
     }
 }
