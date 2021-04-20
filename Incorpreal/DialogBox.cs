f827f0c74cdb5d381ea87625ea960f5c3db05483
Brendan <brockbrendan0@gmail.com>
2021-04-20 02:15:32 -0400

    Some formatting changes.

--- Incorpreal/DialogBox.cs
+++ Incorpreal/DialogBox.cs
@@ -7,29 +7,29 @@ public class DialogBox : CanvasLayer
     //Code inspired by Emilio on YouTube 
     
     //String path to dialogue JSON file
-    [Export] public String dialogPath = "res://Dialogues/Dialog0.txt";
+    [Export] public String DialogPath = "res://Dialogues/Dialog0.txt";
     
     //A string array of all text for current situation
-    protected String[] dialog;
+    protected String[] _dialog;
 
     //An index for the above array
-    protected int dialog_index = 1;
+    protected int dialogIndex = 1;
 
     //A boolean to tell when all text has been displayed
     //and there is nothing left to show
-    private Boolean finished = false;
+    private Boolean _finished = false;
 
     //Speaker name box
-    private RichTextLabel speaker;
+    private RichTextLabel _speaker;
 
     //The actual text box itself
-    private RichTextLabel text_box;
+    private RichTextLabel _textBox;
 
     //The little corner indicator for when text is ready to transition
-    private Sprite next_indicator;
+    private Sprite _nextIndicator;
 
     //The tween that handles animating our text
-    private Tween text_animator;
+    private Tween _textAnimator;
     //The name tween comes from in-betweening, an animation technique where you specify keyframes and the computer interpolates the frames that appear between them -GodotDocumentation
 
     //Empty constructor for other classes
@@ -40,53 +40,52 @@ public class DialogBox : CanvasLayer
     // Called when the node enters the scene tree for the first time.
     public override void _Ready()
     {
-        dialog = getDialog();
-        //Debug.Assert((dialog == null || dialog.Length == 0), "Dialogue not found!");
-        speaker = (RichTextLabel)GetNode("DialogBox/SpeakerBox");
-        text_box = (RichTextLabel)GetNode("DialogBox/TextBox");
-        text_animator = (Tween)GetNode("DialogBox/Tween");
-        next_indicator = (Sprite)GetNode("DialogBox/Next-Indicator");
-        loadDialogue();
+        _dialog = GetDialogue();
+        _speaker = (RichTextLabel)GetNode("DialogBox/SpeakerBox");
+        _textBox = (RichTextLabel)GetNode("DialogBox/TextBox");
+        _textAnimator = (Tween)GetNode("DialogBox/Tween");
+        _nextIndicator = (Sprite)GetNode("DialogBox/Next-Indicator");
+        LoadDialogue();
     }
 
     //Pull the next line of text and play it inside the text box
-    public void loadDialogue() {
-        if(dialog_index < dialog.Length) {
-            finished = false;
-            speaker.BbcodeText = dialog[0];
-            text_box.BbcodeText = dialog[dialog_index];
-            text_box.PercentVisible = 0;
-            text_animator.InterpolateProperty(text_box, "percent_visible", 0, 1, 3, Tween.TransitionType.Linear, Tween.EaseType.InOut);
+    public void LoadDialogue() {
+        if(dialogIndex < _dialog.Length) {
+            _finished = false;
+            _speaker.BbcodeText = _dialog[0];
+            _textBox.BbcodeText = _dialog[dialogIndex];
+            _textBox.PercentVisible = 0;
+            _textAnimator.InterpolateProperty(_textBox, "percent_visible", 0, 1, 2, Tween.TransitionType.Linear, Tween.EaseType.InOut);
             //InterpolateProperty(Object @object, NodePath property, object initialVal, object finalVal, float duration, TransitionType transType, EaseType easeType, float delay = 0f)
-            text_animator.Start();
+            _textAnimator.Start();
         }
 
         else {
             QueueFree();
         }
 
-        dialog_index++;
+        dialogIndex++;
     }
 
     // Called every frame. The value 'delta' is the elapsed time since the previous frame.
     public override void _Process(float delta)
     {
-        next_indicator.Visible = finished;
+        _nextIndicator.Visible = _finished;
         if(Input.IsActionJustPressed("Continue")){
-            loadDialogue();
+            LoadDialogue();
         }
     }
 
     public void onTweenCompleted(Godot.Object obj, NodePath key) {
-        finished = true;
+        _finished = true;
     }
 
-    public String[] getDialog() {
+    public String[] GetDialogue() {
         try {
             File textFile = new File();
-            Debug.Assert(textFile.FileExists(dialogPath), "File not found");
+            Debug.Assert(textFile.FileExists(DialogPath), "File not found");
 
-            textFile.Open(dialogPath, File.ModeFlags.Read);
+            textFile.Open(DialogPath, File.ModeFlags.Read);
 
             var inFile =  textFile.GetAsText();
 
--- Incorpreal/TileSets/Interaction.cs
+++ Incorpreal/TileSets/Interaction.cs
@@ -8,32 +8,32 @@ public class Interaction : CanvasLayer
     //The dialogue box scene for use in code
     PackedScene dialogueBoxes = GD.Load<PackedScene>("res://DialogueBoxV2.tscn");
 
-    //Our "state" for player interaction with map objects,
-    //Will likely be replaced by an Enum as code grows more complex
-    //Enum states = {Idle, Looting, Reading, Puzzle, Secret}
-    String action_state = "off";
-
-    //The loot_areas to be disabled after the chest opens
-    Godot.Collections.Array<Area2D> loot_areas = new Godot.Collections.Array<Area2D>();
-
-    //The collsion shape to turn off
-    CollisionShape2D disable_chest;
-
     //CanvasLayer to hold dialogue box
     DialogBox diagBox;
 
-    // Chests are loaded from left to right, top to bottom. 
-    // Chests in the upper left corner are loaded before lower right.
-    // Tracking number of chests.
-    int current_chest = 0;
+    //Our "state" for player interaction with map objects,
+    //Will likely be replaced by an Enum as code grows more complex
+    //Enum states = {Idle, Looting, Reading, Puzzle, Secret}
+    protected String actionState = "off";
+
+    //The loot areas to be disabled after the chest opens
+    private Godot.Collections.Array<Area2D> _lootAreas = new Godot.Collections.Array<Area2D>();
+
+    //The collsion shape to turn off
+    private CollisionShape2D _currentChest; 
+    
+    /*Chests are loaded from left to right, top to bottom. 
+    Chests in the upper left corner are loaded before lower right.
+    Tracking number of chests. */
+    private int _chestIndex = 0;
 
     //Variables to hold current tilemap, tile texture and tile location
-    TileMap changeMap;
-    Vector2 tile;
-    Vector2 tile_region;
-    Vector2 open_chest = new Vector2(2,0);
-    Godot.Collections.Array<Vector2> usedTiles = new Godot.Collections.Array<Vector2>();
-    Godot.Collections.Array<Vector2> usedTextures = new Godot.Collections.Array<Vector2>();
+    private TileMap _interactiveTilemap;
+    private Vector2 _currentTileLocation;
+    private Vector2 _currentTileTexture;
+    private Vector2 _openChest = new Vector2(2,0);
+    private Godot.Collections.Array<Vector2> _allTileLocations = new Godot.Collections.Array<Vector2>();
+    private Godot.Collections.Array<Vector2> _allTileTextures = new Godot.Collections.Array<Vector2>();
 
     //Empty constructor for use in other scripts
     public Interaction() {
@@ -42,30 +42,30 @@ public class Interaction : CanvasLayer
 
     //Called when the node enters the scene tree for the first time.
     public override void _Ready() {
-        changeMap = (TileMap)GetNode("../Interactables");
+        _interactiveTilemap = (TileMap)GetNode("../Interactables");
         diagBox = dialogueBoxes.Instance() as DialogBox;
     }
 
     public override void _Process(float delta) {
         if(Input.IsActionJustPressed("interact")) {
-            //Respond based on the action_state
-            switch(action_state) {
+            //Respond based on the actionState
+            switch(actionState) {
                 case "off":
                     GD.Print("I got nothing to do here. \n");
                     break;
 
                 case "on": 
                     //Add some function or load a scene/panel for gold, etc.
-                    diagBox.dialogPath = "res://Dialogues/Chest.txt";
-                    GetTree().GetRoot().GetNode("Node2D/Player/Camera2D/").AddChild(diagBox);
-                    changeMap.SetCell((int)tile[0], (int)tile[1], 22, false, false, false, tile_region);
+                    diagBox.DialogPath = "res://Dialogues/Chest.txt";
+                    GetTree().Root.GetNode("Node2D/Player/Camera2D/").AddChild(diagBox);
+                    _interactiveTilemap.SetCell((int)_currentTileLocation[0], (int)_currentTileLocation[1], 22, false, false, false, _currentTileTexture);
                     //SetCell(int x, int y, int tile, boolean flip_x, boolean flip_y, boolean transpose, Vector2 autotileCoordinates)
 
-                    disable_chest = (CollisionShape2D)(GetNode(((String)(loot_areas[current_chest].GetPath() + "/CollisionShape2D"))));
-                    disable_chest.Disabled = true;
+                    _currentChest = (CollisionShape2D)(GetNode(((String)(_lootAreas[_chestIndex].GetPath() + "/CollisionShape2D"))));
+                    _currentChest.Disabled = true;
                     //Disable the collision box for this chest
 
-                    current_chest++;
+                    _chestIndex++;
                     //Switch to next chest
                     break;
 
@@ -77,29 +77,29 @@ public class Interaction : CanvasLayer
     }
 
     //The actual vector2 tile coordiate on the tilemap
-    public void addUsedTiles(Vector2 used_tile) {
-        usedTiles.Add(used_tile);
+    public void AddChestTile(Vector2 used_tile) {
+        _allTileLocations.Add(used_tile);
     }
 
     //The vector2 of tile texture from the tileset
-    public void addUsedTextures(Vector2 used_texture) {
-        usedTextures.Add(used_texture);
+    public void AddChestTextures(Vector2 used_texture) {
+        _allTileTextures.Add(used_texture);
     }
 
     //The loot areas that will be disabled once the chest is opened
-    public void setDisabledLootArea(Area2D openedChest) {
-        loot_areas.Add(openedChest);
+    public void AddOpenedLootAreas(Area2D openedChest) {
+        _lootAreas.Add(openedChest);
     }
     
     //When the player enters a loot_area, print "Looting"
     public void OnLootAreaEntered(Area2D area) {
-        action_state = "on";
-        tile = (Vector2)usedTiles[current_chest]; //Pass the current tile ingame to the process method
-        tile_region = (Vector2)usedTextures[current_chest] + open_chest; //Pass the Vector2 and change the texture to the "open chest" texture
+        actionState = "on";
+        _currentTileLocation = (Vector2)_allTileLocations[_chestIndex]; //Pass the current tile ingame to the process method
+        _currentTileTexture = (Vector2)_allTileTextures[_chestIndex] + _openChest; //Pass the Vector2 and change the texture to the "open chest" texture
     }
 
     //When the player leaves a loot_area, print "Leaving"
     public void OnLootAreaExited(Area2D area) {
-        action_state = "off";
+        actionState = "off";
     }
 }
