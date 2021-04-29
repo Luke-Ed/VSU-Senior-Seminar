441300d35a709261b7219eb1425127ce2bb7fcbd
Aeden <brockbrendan0@gmail.com>
2021-04-28 23:41:06 -0400

    Cleanup commit before merge.

--- Incorpreal/Game.cs
+++ Incorpreal/Game.cs
@@ -3,17 +3,14 @@ using System;
 
 public class Game : Node
 {
-
 	public Game()
 	{
+
 	}
 
-
-	// Declare member variables here. Examples:
-	// private int a = 2;
 	public AudioStreamPlayer audioStreamPlayer = new AudioStreamPlayer();
 	const string Path = "res://sounds/test.wav";
-	// private string b = "text";
+
 	// Called when the node enters the scene tree for the first time.
 	public override void _Ready()
 	{
