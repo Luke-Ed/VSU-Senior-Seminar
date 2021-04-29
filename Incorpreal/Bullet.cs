598f7fe1eeb8bec090ca8373e077e7836c2d1424
Luke Edmondson <luke_ed@outlook.com>
2021-04-28 20:55:43 -0400

    Minor Cleanup on Bullet.cs

--- Incorpreal/Bullet.cs
+++ Incorpreal/Bullet.cs
@@ -1,34 +1,25 @@
 using Godot;
 using System;
 
-public class Bullet : KinematicBody2D
-{
-    Vector2 velocity = new Vector2();
+public class Bullet : KinematicBody2D {
+  Vector2 _velocity;
 
-    // Called when the node enters the scene tree for the first time.
-    public override void _Ready()
-    {
-        velocity.y = -600;
+  // Called when the node enters the scene tree for the first time.
+  public override void _Ready() {
+    _velocity.y = -600;
+  }
+
+  public override void _PhysicsProcess(float delta) {
+    if (Position.y < -1000) {
+      QueueFree();
     }
+    MoveAndSlide(_velocity);
 
-    public override void _PhysicsProcess(float delta)
-    {
-        if (Position.y < -1000)
-        {
-            QueueFree();
-        }
-        MoveAndSlide(velocity);
+    var collision = MoveAndCollide(_velocity.Normalized() * delta);
 
-        var collision = MoveAndCollide(velocity.Normalized() * delta);
-
-        if (collision != null)
-        {
-            if (collision.Collider.HasMethod("Hit"))
-            {
-                collision.Collider.Call("Hit");
-                QueueFree();
-            }
-        }
-
+    if (collision != null && collision.Collider.HasMethod("Hit")) {
+      collision.Collider.Call("Hit");
+        QueueFree();
     }
+  }
 }
