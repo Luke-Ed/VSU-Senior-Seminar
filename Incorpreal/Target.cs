62ba7e6ba55d6bdfad13a02f6b43a67e362cfaa5
Luke Edmondson <luke_ed@outlook.com>
2021-04-29 12:15:44 -0400

    Cleaned up Target.cs

--- Incorpreal/Target.cs
+++ Incorpreal/Target.cs
@@ -1,20 +1,9 @@
 using Godot;
-using System;
 
-public class Target : StaticBody2D
-{
-    // Declare member variables here. Examples:
-    // private int a = 2;
-    // private string b = "text";
-
-    // Called when the node enters the scene tree for the first time.
-    public override void _Ready()
-    {
-        
+namespace Incorpreal {
+  public class Target : StaticBody2D {
+    public void Hit() {
+      Visible = false;
     }
-
-    public void Hit()
-    {
-        Visible = false;
-    }
+  }
 }
