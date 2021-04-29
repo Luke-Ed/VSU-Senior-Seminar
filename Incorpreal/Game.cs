cbd158cc3c9a42316bdd1c4f4a0f5eb259d67dfc
Luke Edmondson <luke_ed@outlook.com>
2021-04-28 21:00:17 -0400

    Removed unnecessary comments.

--- Incorpreal/Game.cs
+++ Incorpreal/Game.cs
@@ -1,33 +1,21 @@
 using Godot;
 using System;
 
-public class Game : Node
-{
-
-    public Game()
-    {
-    }
-
-
-	// Declare member variables here. Examples:
-	// private int a = 2;
+public class Game : Node {
+  public Game() {
+  }
+    
 	public AudioStreamPlayer audioStreamPlayer = new AudioStreamPlayer();
 	const string Path = "res://sounds/test.wav";
-	// private string b = "text";
-	// Called when the node enters the scene tree for the first time.
-	public override void _Ready()
-	{
-		this.AddChild(audioStreamPlayer);
+	
+	public override void _Ready() {
+	  AddChild(audioStreamPlayer);
 		AudioStream Background = (AudioStream)GD.Load(Path);
 		audioStreamPlayer.Stream = Background;
 		audioStreamPlayer.Play();
-		audioStreamPlayer.VolumeDb = (-20);
-		audioStreamPlayer.PauseMode = Node.PauseModeEnum.Process; //This line keeps music playing during pause
+		audioStreamPlayer.VolumeDb = -20;
+		audioStreamPlayer.PauseMode = PauseModeEnum.Process; 
+    //This line keeps music playing during pause
 	}
-
-//  // Called every frame. 'delta' is the elapsed time since the previous frame.
-//  public override void _Process(float delta)
-//  {
-//      
-//  }
+  
 }
