e8232fbe04bc880ad3aa7a6e09c2f225f5fd75e3
Luke Edmondson <luke_ed@outlook.com>
2021-04-29 21:43:37 -0400

    Minor spacing fix.

--- Incorpreal/Enemies/Bat.cs
+++ Incorpreal/Enemies/Bat.cs
@@ -100,14 +100,14 @@ namespace Incorpreal.Enemies {
 
     //After the player gets out of range of the enemy and will continue to stay in that spot after 10 seconds if the enemy is no longer on screen then it is returned to the original starting position
     //and follows the original path if it has one by removing it from the "Following" group. If the enemy is still on screen the timer refreshes and will check again in another 10 seconds.
-      public void onTimeout() {
-        if (OnCamera) {
-          _timer.Start();
-        }
-        else {
-          Position = _startingPos;
-          RemoveFromGroup("Following");
-        }
+    public void onTimeout() {
+      if (OnCamera) {
+        _timer.Start();
       }
+      else {
+        Position = _startingPos;
+        RemoveFromGroup("Following");
+      }
+    }
   }
 }
