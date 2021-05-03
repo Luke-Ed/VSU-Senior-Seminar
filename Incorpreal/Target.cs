14cf33784cf26cb86517b1309d491b78b5a2cff6
Austin Montgomery <austinhunterm@gmail.com>
2021-05-03 12:51:28 -0400

    more sounds

--- Incorpreal/Target.cs
+++ Incorpreal/Target.cs
@@ -6,15 +6,18 @@ public class Target : StaticBody2D
     // Declare member variables here. Examples:
     // private int a = 2;
     // private string b = "text";
-
+    public AudioStreamPlayer hit = new AudioStreamPlayer();
+    AudioStream footstep = (AudioStream)GD.Load("res://sounds/SkeletonHitSound.wav");
     // Called when the node enters the scene tree for the first time.
     public override void _Ready()
     {
-        
+        this.AddChild(hit);
+        hit.Stream=footstep;
     }
 
     public void Hit()
     {
         Visible = false;
+        hit.Play();
     }
 }
