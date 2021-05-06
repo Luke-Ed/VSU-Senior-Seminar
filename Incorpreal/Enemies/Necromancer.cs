fa1a087490ad1621734fa39bc5b4d6806979a046
Luke Edmondson <luke_ed@outlook.com>
2021-05-05 20:04:27 -0400

    Added Necromancer.cs, relinked to Necromancer.tscn

---
+++ Incorpreal/Enemies/Necromancer.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Incorpreal.Enemies {
+  public class Necromancer : AbstractEnemy {
+    public Necromancer() : base(100, 5, 100, "Necromancer", String.Empty) {
+    }
+    
+    protected override void ApplyStatusEffect() {
+      
+    }
+  }
+}
